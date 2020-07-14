2b7cb789713607edab41e94db405d6bc95f9a0da
p479h <30977782+p479h@users.noreply.github.com>
2020-07-14 22:06:24 +0200

    Update pendulum.js
    Added funcitonality

--- JS_Simulations/pendulum.js
+++ JS_Simulations/pendulum.js
@@ -1,31 +1,15 @@
 //First the constants and other values
-let Ø; //Angle between vertical and string. Torwards x ax is positive
-let dØ; //Infinitesimal change in angle
-let t; //Time since beggining of the simulation
-let dt; //Time step
-let x; //X coord, which will be updated if the thingy moves!
-let y; //y coord, which will be updated if the thingy moves!
-let v; //SPEED not velocity
-let F; //The external force on the mass (gravity)
-let l; //Length of the spring
-let m; //Mass of the object
-let runFlag; //Controls whether time is allowed to pass or not.
+let Ø, dØ, t, dt, F, x, y, v, l, m; //Parms and dummy vars
+let runFlag; //Flag to check if simulation runs
 
 //Now onto the widgets
-let W; //Canvas width
-let H; //Canvas height
-let sldt; //Slider for dt
-let slLen; //Slider length of spring
-let slM; //Slider mass of the object
-let slØ; //SLider angle
-let startBut; //Start animation button
-let replBut; //Button to reposition elements
-let pauseBut; //Stop the animation at a given time
+let W = 500, H = 400; //Canvas height and width
+let sldt,slLen, slM, slØ; //Sliders
+let startBut,replBut,pauseBut; //Buttons
 let xSLI; //X position of sliders
 let canvas; //Canvas in case a canvas "instance" is returned
-let bg; //Background color [r,g,b]
-let fontColor; //Color fo the font
-
+let bg = [0, 0, 0]; //Background color [r,g,b]
+let fontColor = [10, 10, 10, 255]; //Color fo the font
 
 function resetValues(){
   //THis function resets constants to default
@@ -48,41 +32,6 @@ function resetValues(){
   H = 400;
 };
 
-/*Now we make a callback generator for the widgets
-This is made in an entirely separate function due to
-organization purpuses.
-
-The input is the name of the widget. It returns a function
-that performs the action the widget is to do.*/
-function callback(widName){
-  let s = widName
-  if (s == "sldt"){
-    return function(){dt = sldt.value();};
-  }
-  else if (s == "slM"){
-    return function(){m = slM.value();};
-  }
-  else if (s == "slLen"){
-    return function(){l = slLen.value();};
-  }
-  else if (s == "slØ"){
-    return function(){Ø = slØ.value();};
-  }
-  else if (s == "startBut"){
-    return function(){runFlag = true};
-  }
-  else if (s == "pauseBut"){
-    return function(){runFlag = false};
-  }
-  else if (s == "replBut"){
-    return function(){resetValues();};
-  }
-  else{
-    alert("UNRECOGNIZED WIDZgEt");
-  }
-};
-
-
 /*Now we make a function to attribute values to the
 constants above when used the first time.
 Else, it resets everything to initial situation.*/
@@ -90,10 +39,7 @@ function setup() {
   //Check if the funcition is being used for first time
   if (sldt == undefined || sldt == null) {
     //Making the canvas
-    W = 500;
-    H = 400;
     canvas = createCanvas(W, H);
-    bg = [0, 0, 0];
     background(bg);
 
     //Improves the looks
@@ -103,10 +49,9 @@ function setup() {
     xSLI = 10;
 
     //Setting fontcolor
-    fontColor = [200, 255, 255, 255];
     fill(fontColor);
     textSize(14);
-    textFont("Consolas")
+    textFont("Consolas");
 
     //Dt slider first
     sldt = createSlider(0.001, 1, 0.001, 0.001);
@@ -115,20 +60,41 @@ function setup() {
 
     //Then make the chord length slider
     slLen = createSlider(10, 300, 200, 1);
-    slLen.position(xSLI, 40);
+    slLen.position(xSLI, 36);
     slLen.style('width', '80px');
 
     //Then make the slider with the mass
     slM = createSlider(0.1, 200, 100, 1);
-    slM.position(xSLI, 70);
+    slM.position(xSLI, 62);
     slM.style('width', '80px');
 
     //Make the Ø slider
     slØ = createSlider(0, PI / 2, PI / 4, 0.01);
-    slØ.position(xSLI, 100);
+    slØ.position(xSLI, 88);
     slØ.style('width', '80px');
 
+    startBut = createButton("Begin");
+    startBut.position(220, 10);
+    startBut.mousePressed(
+      function play(){
+        v=0;
+        Ø = slØ.value();
+        runFlag = true;
+      });
 
+    pauseBut = createButton("Pause");
+    pauseBut.position(220, 45);
+    pauseBut.mousePressed(
+      function pause(){
+        runFlag = false;
+      });
+
+    replBut = createButton("Replay");
+    replBut.position(220, 80);
+    replBut.mousePressed(
+      function replay(){
+        resetValues();
+      });
 
   };
   //With the sliders in place we can set the initial values
@@ -136,6 +102,13 @@ function setup() {
 };
 
 function draw() {
+  //Resetting background
+  background(bg);
+
+  //Drawing boxes
+  fill(192,192,192)
+  rect(0,0,W/1.6, H/3.5);
+
   //Resetting constants
   dt = sldt.value();
   m = slM.value();
@@ -143,27 +116,30 @@ function draw() {
   x = l*sin(Ø);
   y = l*cos(Ø);
 
-  //Resetting background
-  background(bg);
   //Redrawing text
   fill(fontColor);
   noStroke();
-  text("dt: "+dt+" s",xSLI+90, 10+15);
-  text("l: "+l+" pxs",xSLI+90, 40+15);
-  text("Ø: "+slØ.value()+" rad",xSLI+90, 100+15);
-  text("M: "+m+" kg",xSLI+90, 70+15);
+  text("dt: "+dt+" s",xSLI+90, 8+15);
+  text("l: "+l+" pxs",xSLI+90, 35+15);
+  text("Ø: "+slØ.value()+" rad",xSLI+90, 85+15);
+  text("M: "+m+" kg",xSLI+90, 60+15);
 
-  text("t: "+t.toFixed(2)+" s",xSLI+200, 10+15);
-  text("T: "+(2*PI*(l/F)**0.5).toFixed(2)+" s",xSLI+200, 40+15);
+  //time label
+  textSize(17);
+  fill(255);
+  text("t: "+t.toFixed(2)+" s",xSLI+320, 10+15);
+  text("EK: "+(0.5*m*v**2).toFixed(0)+" J",xSLI+320, 37+15);
+  text("EP: "+(m*l*(1-cos(Ø))*F).toFixed(0)+" J",xSLI+320, 65+15);
+  text("EK+EP: "+(0.5*m*v**2+m*l*(1-cos(Ø))*F).toFixed(0)+" J",xSLI+320, 92+15);
 
   //Arc with angle
-  translate(W/2, H/5);
+  translate(W/3, H/3);
   noFill();
   textSize(18);
   stroke([255, 255, 255, 200]);
   strokeWeight(2);
   fill([200, 249, 200, 200]);
-  if (Ø>0){
+  if (sin(Ø)>0){
     arc(0, 0, l/2, l/2, -Ø+3.14/2, 3.14/2);
   } else {arc(0, 0, l/2, l/2,3.14/2,-Ø+3.14/2);}
 
@@ -188,6 +164,11 @@ function draw() {
   line(0, 4*l/20, 0, 5*l/20);
   line(0, 6*l/20, 0, 7*l/20);
 
+  makeAxis(170, 90, 105, 85, "t", "E");
+  strokeWeight(2);
+  makeAxis(170, 210, 105, 85, "t", "Ø");
+  textSize(13);
+  strokeWeight(2);
 
   strokeWeight(2)
   //Drawing mass
@@ -199,4 +180,43 @@ function draw() {
     Ø += v*dt/l;
     t += dt
   };
+};
+
+function makeAxis(x, y, w, h, xlabel, ylabel){
+  line(x, y, x+w, y);
+  line(x, y, x, y-h);
+  triangle(x+w, y+h/20, x+w, y-h/20, x+w+w/20, y);
+  triangle(x-w/20, y-h, x+w/20, y-h, x, y-h-h/20);
+  textSize(10);
+  strokeWeight(0.1);
+  text(xlabel, x+0.9*w, y+h/4);
+  text(ylabel, x-w/6, y-h*0.9);
+};
+
+function getMax(arr){
+  //Get's maximun and minimun in array for normalization
+  //purpuses
+  let max, min;
+  max = arr[0]; min = arr[0];
+  for (let i=0; i<arr.length;i++){
+    max = (arr[i]>max)? arr[i]:max;
+    min = (arr[i]<min)? arr[i]:min;
   };
+  max = (typeof max == "number")? max:arr[0];
+  min = (typeof min == "number")? min:arr[0];
+  return [min, max];
+};
+
+function normalize(arr, scale){
+  let fictitious = []; //Dummy array
+  maxmin = getMax(arr);
+  Min = maxmin[0];
+  Max = maxmin[1];
+  if (Max-Min!=0){
+    for (let i=0; i<arr.length;i++){
+      fictitious.push((arr[i]-Min)*scale/(Max-Min))
+    };
+    return fictitious
+    } else {
+      return arr};
+};
